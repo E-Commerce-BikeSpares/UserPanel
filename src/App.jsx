903de68d7f47f1edf1b0949e2dbaffa194a954cb
dellibabus <sdellibabu8@gmail.com>
2025-02-13 21:11:57 +0530

    `Added RidingGear component and route to App.jsx`

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/HomePage";
 import NotFoundPage from "./Components/ReusableComponents/PageNotFound";
+import RidingGear from "./Components/HomeComponents/RidingComponent";
 
 const AppComponent = () => {
     return (
         <Routes>
           <Route path="*" element={<NotFoundPage/>} />
           <Route index element={<Home/>}/>
+          <Route path="/r" element={<RidingGear/>}/>
           {/* <Route path="/bike" element={<BikeCardMain/>}/> */}
         </Routes>
     );
